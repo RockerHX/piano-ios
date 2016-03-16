45e240c76bdc85109968d4a7261aa9187c790b5a
RockerHX <RockerHX@gmail.com>
2016-03-16 11:14:28 +0800

    新建工程

---
+++ Piano/Piano/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Piano
+//
+//  Created by miaios on 16/3/16.
+//  Copyright © 2016年 Mia Music. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
