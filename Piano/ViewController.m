6c56167bcc118de06950239e7a4a1ce3bbe76b9c
RockerHX <RockerHX@gmail.com>
2016-03-17 20:33:51 +0800

    获取直播房间列表

--- Piano/ViewController.m
+++ Piano/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "MiaAPIHelper.h"
+#import "WebSocketMgr.h"
 
 @interface ViewController ()
 
@@ -17,11 +19,72 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+	[self loadConfigure];
+}
+
+- (void)dealloc {
+	// Socket
+	[[NSNotificationCenter defaultCenter] removeObserver:self name:WebSocketMgrNotificationDidOpen object:nil];
+	[[NSNotificationCenter defaultCenter] removeObserver:self name:WebSocketMgrNotificationDidFailWithError object:nil];
+	[[NSNotificationCenter defaultCenter] removeObserver:self name:WebSocketMgrNotificationDidAutoReconnectFailed object:nil];
+	[[NSNotificationCenter defaultCenter] removeObserver:self name:WebSocketMgrNotificationDidCloseWithCode object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Config Methods
+- (void)loadConfigure {
+	[[WebSocketMgr standard] watchNetworkStatus];
+
+	// Socket
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notificationWebSocketDidOpen:) name:WebSocketMgrNotificationDidOpen object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notificationWebSocketDidFailWithError:) name:WebSocketMgrNotificationDidFailWithError object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notificationWebSocketDidAutoReconnectFailed:) name:WebSocketMgrNotificationDidAutoReconnectFailed object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(notificationWebSocketDidCloseWithCode:) name:WebSocketMgrNotificationDidCloseWithCode object:nil];
+}
+
+#pragma mark - Socket
+- (void)notificationWebSocketDidOpen:(NSNotification *)notification {
+	[MiaAPIHelper sendUUIDWithCompleteBlock:^(MiaRequestItem *requestItem, BOOL success, NSDictionary *userInfo) {
+		if (success) {
+			[self fetchRoomList];
+		} else {
+			NSLog(@"sendUUID failed");
+		}
+	} timeoutBlock:^(MiaRequestItem *requestItem) {
+		NSLog(@"sendUUID timeout");
+	}];
+}
+
+- (void)notificationWebSocketDidFailWithError:(NSNotification *)notification {
+	NSLog(@"notificationWebSocketDidFailWithError");
+}
+
+- (void)notificationWebSocketDidAutoReconnectFailed:(NSNotification *)notification {
+	NSLog(@"notificationWebSocketDidAutoReconnectFailed");
+}
+
+- (void)notificationWebSocketDidCloseWithCode:(NSNotification *)notification {
+	NSLog(@"Connection Closed! (see logs)");
+}
+
+
+#pragma mark - Private Method
+- (void)fetchRoomList {
+	[MiaAPIHelper getRoomListWithCompleteBlock:^(MiaRequestItem *requestItem, BOOL success, NSDictionary *userInfo) {
+		if (success) {
+			NSArray *roomList = userInfo[@"v"][@"data"];
+
+			NSLog(@"%@", roomList);
+		} else {
+			NSLog(@"getRoomList failed");
+		}
+	} timeoutBlock:^(MiaRequestItem *requestItem) {
+		NSLog(@"getRoomList timeout");
+	}];
 }
 
 @end
