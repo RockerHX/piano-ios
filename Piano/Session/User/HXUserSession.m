e03570c4f0ee85e0800629abf143a30703e15760
RockerHX <RockerHX@gmail.com>
2016-03-30 10:35:54 +0800

    guest uid

--- Piano/Session/User/HXUserSession.m
+++ Piano/Session/User/HXUserSession.m
@@ -75,7 +75,11 @@ typedef void(^FailureBlock)(NSString *);
 }
 
 - (NSString *)uid {
-    return _user.uid;
+	if (self.state == HXUserStateLogout) {
+		return self.guestUID;
+	} else {
+		return _user.uid;
+	}
 }
 
 #pragma mark - Public Methods
