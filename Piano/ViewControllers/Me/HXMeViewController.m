3136e0e42a4f90608e3b81290ae12a39543b29cb
RockerHX <RockerHX@gmail.com>
2016-04-01 15:20:07 +0800

    Update

--- Piano/ViewControllers/Me/HXMeViewController.m
+++ Piano/ViewControllers/Me/HXMeViewController.m
@@ -45,7 +45,7 @@ HXMeContainerViewControllerDelegate
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    [self.navigationController setNavigationBarHidden:YES animated:NO];
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
