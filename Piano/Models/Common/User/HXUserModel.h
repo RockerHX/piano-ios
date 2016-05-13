8a250effabde7159f3da8d44f970ccc35df55d48
RockerHX <RockerHX@gmail.com>
2016-05-13 10:38:14 +0800

    完善：用户登录数据

--- Piano/Models/Common/User/HXUserModel.h
+++ Piano/Models/Common/User/HXUserModel.h
@@ -18,6 +18,8 @@
 @property (nonatomic, strong)   NSString *token;
 @property (nonatomic, strong)   NSString *nickName;
 @property (nonatomic, strong)   NSString *avatarUrl;
+@property (nonatomic, strong)   NSString *coverUrl;
+@property (nonatomic, strong)   NSString *videoUrl;
 @property (nonatomic, strong)   NSString *notifyAvatar;
 
 @end
