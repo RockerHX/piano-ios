056f4983dd0be329ec3e6b8c0379cb4ecbb15ca3
RockerHX <RockerHX@gmail.com>
2016-05-16 11:27:36 +0800

    Open Zego Log

--- Piano/Session/ZegoAVKit/HXZegoAVKitManager.m
+++ Piano/Session/ZegoAVKit/HXZegoAVKitManager.m
@@ -44,6 +44,7 @@
 - (void)initConfigure {
 //    _zegoLiveApi = [[ZegoLiveApi alloc] initWithAppID:1 appSignature:[self zegoAppSignFromServer]];
     _zegoLiveApi = [[ZegoLiveApi alloc] initWithAppID:1533641591 appSignature:[self zegoAppSignFromServer]];
+    [ZegoLiveApi setLogLevel:4];
 }
 
 #pragma mark - Public Methods
