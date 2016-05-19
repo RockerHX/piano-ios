d3eb7fc789c93dd283e67235d87416f1bfa1ef9c
RockerHX <RockerHX@gmail.com>
2016-05-19 17:48:13 +0800

    Add HXGiftModel

---
+++ Piano/Models/Common/Gift/HXGiftModel.h
@@ -0,0 +1,19 @@
+//
+//  HXGiftModel.h
+//  Piano
+//
+//  Created by miaios on 16/5/19.
+//  Copyright © 2016年 Mia Music. All rights reserved.
+//
+
+#import "MJExtension.h"
+
+
+@interface HXGiftModel : NSObject
+
+@property (nonatomic, assign) NSInteger  status;
+@property (nonatomic, assign) NSInteger  mcoin;
+@property (nonatomic, strong)  NSString *ID;
+@property (nonatomic, strong)  NSString *name;
+
+@end
