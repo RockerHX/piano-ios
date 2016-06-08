e2ec3d4c76e5ad330b8969d56880fc1b628a9743
RockerHX <RockerHX@gmail.com>
2016-06-08 18:27:42 +0800

    修正：举报交互文案

--- Piano/ViewControllers/Live/Watch/HXWatchLiveViewController.m
+++ Piano/ViewControllers/Live/Watch/HXWatchLiveViewController.m
@@ -222,14 +222,10 @@ HXLiveAlbumViewDelegate
 }
 
 - (void)reportAnchorWithID:(NSString *)uid {
-    [UIAlertView bk_showAlertViewWithTitle:@"是否举报主播" message:nil cancelButtonTitle:@"取消" otherButtonTitles:@[@"举报"] handler:^(UIAlertView *alertView, NSInteger buttonIndex) {
-        if (buttonIndex != alertView.cancelButtonIndex) {
-            [MiaAPIHelper reportWithType:@"resport_anchor" content:uid completeBlock:^(MiaRequestItem *requestItem, BOOL success, NSDictionary *userInfo) {
-                [self showBannerWithPrompt:@"举报成功"];
-            } timeoutBlock:^(MiaRequestItem *requestItem) {
-                [self showBannerWithPrompt:@"网络超时，举报失败"];
-            }];
-        }
+    [MiaAPIHelper reportWithType:@"resport_anchor" content:uid completeBlock:^(MiaRequestItem *requestItem, BOOL success, NSDictionary *userInfo) {
+        [self showBannerWithPrompt:@"举报成功"];
+    } timeoutBlock:^(MiaRequestItem *requestItem) {
+        [self showBannerWithPrompt:@"网络超时，举报失败"];
     }];
 }
 
