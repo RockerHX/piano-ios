dcbd9f2994c53363d269a0e087f8377b1739cca2
RockerHX <RockerHX@gmail.com>
2016-06-12 16:41:48 +0800

    完善：主播在直播时，app退后台，需要发送系统弹幕，wording为：主播暂时离开一下，马上回来~

--- Piano/Models/Common/Barrage/HXBarrageModel.m
+++ Piano/Models/Common/Barrage/HXBarrageModel.m
@@ -25,29 +25,33 @@
     _type = type;
     NSString *actionString = nil;
     switch (type) {
-            case HXBarrageTypeEnter: {
-                actionString = @"进入了直播间";
-                break;
-            }
-            case HXBarrageTypeAttention: {
-                actionString = @"关注了主播";
-                break;
-            }
-            case HXBarrageTypeShare: {
-                actionString = @"分享了本次直播";
-                break;
-            }
-            case HXBarrageTypeGift: {
-                actionString = [NSString stringWithFormat:@"赠送给主播%@", _giftName];
-                break;
-            }
-            case HXBarrageTypeReward: {
-                actionString = [NSString stringWithFormat:@"打赏了主播个人专辑%@M币", @(_rewardCoinCount).stringValue];
-                break;
-            }
-            default: {
-                break;
-            }
+        case HXBarrageTypeEnter: {
+            actionString = @"进入了直播间";
+            break;
+        }
+        case HXBarrageTypeAttention: {
+            actionString = @"关注了主播";
+            break;
+        }
+        case HXBarrageTypeShare: {
+            actionString = @"分享了本次直播";
+            break;
+        }
+        case HXBarrageTypeGift: {
+            actionString = [NSString stringWithFormat:@"赠送给主播%@", _giftName];
+            break;
+        }
+        case HXBarrageTypeReward: {
+            actionString = [NSString stringWithFormat:@"打赏了主播个人专辑%@M币", @(_rewardCoinCount).stringValue];
+            break;
+        }
+        case HXBarrageTypeBackEnd: {
+            actionString = @"主播暂时离开一下，马上回来~";
+            break;
+        }
+        default: {
+            break;
+        }
     }
     _prompt = [_nickName stringByAppendingString:actionString];
 }
